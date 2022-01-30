1d21fc5d1ca08671a4357e00b7992aee06b298e7
AmuMundhare208 <amundhare123@gmail.com>
2022-01-30 08:22:17 +0530

    my ist commit

---
+++ src/main/java/Epi_Test/Test/App.java
@@ -0,0 +1,13 @@
+package Epi_Test.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Epi_Test/Test/t1.java
@@ -0,0 +1,8 @@
+package Epi_Test.Test;
+
+public class t1 {
+
+	public static void main(String a[]) {
+		System.out.println("THid is my test");
+	}
+}
